c4252a165940cc4ad8c265e0045c555d96b243a9
zolangg <87374129+zolangg@users.noreply.github.com>
2025-08-05 23:09:06 +0200

    Update stock_ranking_app.py

--- stock_ranking_app.py
+++ stock_ranking_app.py
@@ -304,15 +304,15 @@ if "prev_news_weight" not in st.session_state or st.session_state["prev_news_wei
 st.session_state["prev_news_weight"] = news_weight
 
 def heat_level(score):
-    if score >= 6.7:
+    if score >= 6.5:
         return "A++"
-    elif score >= 6.2:
+    elif score >= 6:
         return "A+"
-    elif score >= 5.5:
+    elif score >= 5.3:
         return "A"
-    elif score >= 4.7:
+    elif score >= 4.5:
         return "B"
-    elif score >= 3.7:
+    elif score >= 3.5:
         return "C"
     else:
         return "D"
