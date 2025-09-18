a9d03501fddee50aada45b161cbf47415fff765b
zolangg <87374129+zolangg@users.noreply.github.com>
2025-09-18 18:06:30 +0200

    Update install.R

--- install.R
+++ install.R
@@ -1,9 +1,2 @@
-# install.R  (put at the repo root)
-pkgs <- c(
-  "jsonlite",   # required by predict_bart_cli.R
-  "dbarts",     # needed to predict with bart_model_A_*.rds
-  "BART",       # needed to predict with bart_model_B_*.rds
-  "pROC",       # only if you ever compute AUCs in R
-  "caret"       # only if you refit caret models later
-)
+pkgs <- c("jsonlite", "dbarts", "BART", "pROC", "caret")
 install.packages(pkgs, repos = "https://cloud.r-project.org")
