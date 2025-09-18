b9089f0091a5d35775ae30fb5b725e52019ee112
zolangg <87374129+zolangg@users.noreply.github.com>
2025-09-18 19:17:45 +0200

    Delete install.R

--- install.R
+++
@@ -1,5 +0,0 @@
-# install.R
-options(repos = c(CRAN = "https://cloud.r-project.org"))
-pkgs <- c("jsonlite","dbarts","BART","pROC")
-to_install <- setdiff(pkgs, rownames(installed.packages()))
-if (length(to_install)) install.packages(to_install, Ncpus = 2)
